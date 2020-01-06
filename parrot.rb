1971078b4dbb7580b2a6b4d5560f2dc76a8bebb5
Jared Fleming <jaredmf10@gmail.com>
2020-01-06 15:36:33 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(says = "Squawk!")
+  # pass into method parrot
   puts says
+  # output to screen
   says
+  # returns
 end
